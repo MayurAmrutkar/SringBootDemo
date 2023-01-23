722d311850b9522bc5bb629bb8419f1780f46085
MayurAmrutkar <123346310+MayurAmrutkar@users.noreply.github.com>
2023-01-23 12:48:07 +0530

    SringBootDemo application

---
+++ SringBootDemo/src/main/java/com/example/demo/SringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringBootDemoApplication.class, args);
+	}
+
+}
